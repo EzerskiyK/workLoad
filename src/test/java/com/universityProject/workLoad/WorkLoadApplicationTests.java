98f2ca703e27741b11d9c2711c6ce43028e39305
Kirill Yezerskiy <ezerskiyk@gmail.com>
2025-07-13 09:43:31 +0300

    TASK-1 : add ci jenkins

--- src/test/java/com/universityProject/workLoad/WorkLoadApplicationTests.java
+++ src/test/java/com/universityProject/workLoad/WorkLoadApplicationTests.java
@@ -1,5 +1,6 @@
 package com.universityProject.workLoad;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,10 @@ class WorkLoadApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	void simpleTest() {
+		Assertions.assertEquals(0, 0);
 	}
 
 }
