5d94da203ac46c0e756a924cedf63cd94e64f20a
Kirill Yezerskiy <ezerskiyk@gmail.com>
2025-07-13 10:51:30 +0300

    add h2 for test and test profile

--- src/test/java/com/universityProject/workLoad/WorkLoadApplicationTests.java
+++ src/test/java/com/universityProject/workLoad/WorkLoadApplicationTests.java
@@ -3,14 +3,17 @@ package com.universityProject.workLoad;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class WorkLoadApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
 	void simpleTest() {
 		Assertions.assertEquals(0, 0);
 	}
